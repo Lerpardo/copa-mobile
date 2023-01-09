9112a9ff67682552574380912c602edcdc0079b6
Lerpardo <ls6182315@gmail.com>
2023-01-09 15:13:48 -0300

    fix

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,12 @@
 import { Text, Center } from "native-base";
-
-import Logo from '../assets/logo.svg'
+import Logo from "../assets/logo.svg";
 import Button from "../components/Button";
+
 export function SignIn() {
-    return (
-        <Center flex={1} bgColor="gray.900">
-        <Logo width={212} height={40}/>
-        <Button title="ENTRAR COM GOOGLE"/>
-      </Center>
-    )
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Logo width={212} height={40} />
+      <Button title="ENTRAR COM GOOGLE" />
+    </Center>
+  );
 }
