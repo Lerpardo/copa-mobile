d12454f4ad9ac0a10a4cef3a36b3237c3c9e5c37
Lerpardo <ls6182315@gmail.com>
2023-01-09 19:10:45 -0300

    set user props

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 import * as Google from "expo-auth-session/providers/google";
 import * as AuthSession from "expo-auth-session";
 import * as WebBrowser from "expo-web-browser";
@@ -12,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
@@ -21,6 +22,9 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+    const [user,setUser] = useState<UserProps>({} as UserProps)
+  const [isUserLoading, setIsUserLoading] = useState(false);
+
   const [request, response, promptAsync] = Google.useAuthRequest({
     clientId:
       "459959545530-3ctc3ahodpqj74u2fe6mbbinnm89si08.apps.googleusercontent.com",
@@ -29,17 +33,23 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
   });
 
   async function signIn() {
-    console.log("Entrou");
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+    } catch (e) {
+      console.log(e.message);
+      throw e;
+    } finally {
+      setIsUserLoading(false);
+    }
   }
-  
+
   return (
     <AuthContext.Provider
       value={{
         signIn,
-        user: {
-          name: "Beterraba",
-          avatarUrl: "https://github.com/Lerpardo.png",
-        },
+        isUserLoading,
+        user,
       }}
     >
       {children}
