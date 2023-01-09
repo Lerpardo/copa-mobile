28295b3eb101f21c4c8f900fc36d799ff17a6eca
Lerpardo <ls6182315@gmail.com>
2023-01-09 16:29:50 -0300

    rest add

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,14 @@ import { Button as ButtonNativeBase, Text, IButtonProps } from "native-base";
 
 interface Props extends IButtonProps {
   title: string;
+  type?:'PRIMARY' | 'SECONDARY'
 }
 
-function Button({ title }: Props) {
+function Button({ title, ...rest }: Props) {
   return (
-    <ButtonNativeBase>
+    <ButtonNativeBase
+
+     { ...rest }>
       <Text>{title}</Text>
     </ButtonNativeBase>
   );
