2702416f3c9b752f3ede0e72bd12c1bee5cc160c
Lerpardo <ls6182315@gmail.com>
2023-01-09 17:59:30 -0300

    import auth hook

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,24 +1,28 @@
 import { Text, Center, Icon } from "native-base";
 import { Fontisto } from "@expo/vector-icons";
+import { useAuth } from "../hooks/useAuth";
 
 import Logo from "../assets/logo.svg";
 
 import Button from "../components/Button";
 
 export function SignIn() {
+  const { signIn,user } = useAuth();
+  console.log('dados:',user);
+  
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
       <Button
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         mt={12}
+        onPress={signIn}
         title="ENTRAR COM GOOGLE"
         type="SECONDARY"
       />
       <Text color="white" textAlign="center" mt={4}>
-        Não utilizamos nenhuma informação além {'\n'}
-        do seu e-mail para criação de sua
-        conta.
+        Não utilizamos nenhuma informação além {"\n"}
+        do seu e-mail para criação de sua conta.
       </Text>
     </Center>
   );
