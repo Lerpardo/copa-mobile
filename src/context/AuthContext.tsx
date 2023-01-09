8dadce116c9a50a6337db1bc5499f29813c85a4d
Lerpardo <ls6182315@gmail.com>
2023-01-09 18:45:51 -0300

    Google auth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,9 @@
 import { createContext, ReactNode } from "react";
+import * as Google from 'expo-auth-session/providers/google'
+import * as AuthSession from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -11,14 +16,19 @@ export interface AuthContextDataProps {
 }
 
 interface AuthProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 export const AuthContext = createContext({} as AuthContextDataProps);
 
-export function AuthContextProvider({ children } : AuthProviderProps) {
+export function AuthContextProvider({ children }: AuthProviderProps) {
+    Google.useAuthRequest({
+        clientId: '459959545530-3ctc3ahodpqj74u2fe6mbbinnm89si08.apps.googleusercontent.com',
+        redirectUri:AuthSession.makeRedirectUri({ useProxy: true }),
+        scopes: ['profile','email']
+    })
+  console.log(AuthSession.makeRedirectUri({ useProxy: true }));
   async function signIn() {
-    console.log('Entrou');
-    
+    console.log("Entrou");
   }
   return (
     <AuthContext.Provider
